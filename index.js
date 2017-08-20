f4b851a611aa380c1406ef83ec3b1fb1b2e3f0e6
javadoug <doug@onvelocity.com>
2017-08-19 23:43:51 -0400

    add babel prepare

--- index.js
+++
@@ -1,7 +0,0 @@
-
-export forgotPassword from './src/forgotPassword';
-export forgotPasswordConfirm from './src/forgotPasswordConfirm';
-export signUp from './src/signUp';
-export signUpConfirm from './src/signUpConfirm';
-export signUpConfirmResend from './src/signUpConfirmResend';
-export getUsernameFromEmail from './src/getUsernameFromEmail';
---
+++ src/index.js
@@ -0,0 +1,7 @@
+
+export {forgotPassword} from './forgotPassword';
+export {forgotPasswordConfirm} from './forgotPasswordConfirm';
+export {signUp} from './signUp';
+export {signUpConfirm} from './signUpConfirm';
+export {signUpConfirmResend} from './signUpConfirmResend';
+export {getUsernameFromEmail} from './getUsernameFromEmail';
